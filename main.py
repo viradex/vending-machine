b4db268d7d29dd0d0a1517c1792fd68819c140b1
arnavthorat78 <arnavthorat78@gmail.com>
2026-02-04 21:12:06 +1100

    Add coin stock system

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ items = {
 }
 
 # In form: { value: stock }
-min_value = 5
-max_value = 15
+min_value = 2
+max_value = 5
 coins = {
 	200: randint(min_value, max_value),
 	100: randint(min_value, max_value),
@@ -36,7 +36,7 @@ price = items[item]
 print(f"Item {item} costs ${price / 100:.2f}!\n")
 
 print("Accepted coins:")
-for coin in coins.keys():
+for coin in sorted(coins.keys(), reverse=True):
 	if coin >= 100:
 		print(f"{coin}¢ (${int(coin/100)})")
 	else:
@@ -64,34 +64,44 @@ while True:
 	coins[pay] += 1
 
 	if total_paid >= price:
-		print(f"Thanks, you paid for item {item} for ${total_paid/100:.2f}!")
+		print(f"Thanks! Total inserted: ${total_paid/100:.2f}!")
 		break
 
-	print(f"Amount paid so far: ${total_paid/100:.2f} / ${price:.2f}")
+	print(f"Amount paid so far: ${total_paid/100:.2f} / ${price/100:.2f}")
 
 change = []
 change_due = total_paid - price
 
-if not len(change):
+temp_coins = coins.copy()
+temp_change = []
+temp_due = change_due
+
+if change_due > 0:
 	print(f"Receiving change: ${change_due/100:.2f}")
 
 print()
 
-for coin in coins.keys():
-	while change_due >= coin and coins[coin] > 0:
-		change_due -= coin
-		coins[coin] -= 1
+for coin in sorted(temp_coins.keys(), reverse=True):
+	while temp_due >= coin and temp_coins[coin] > 0:
+		temp_due -= coin
+		temp_coins[coin] -= 1
 
-		change.append(coin)
+		temp_change.append(coin)
 
-if change_due > 0:
-	print(f"Sorry, we were not able to give you exact change. Please accept this refund of ${total_paid/100:.2f}.")
+if temp_due > 0:
+    print(f"Sorry, we were not able to give exact change.")
+    print(f"Refunding ${total_paid/100:.2f}...")
+    exit(0)
+
+coins = temp_coins
+change = temp_change
+
 if not len(change):
     print("Thanks for paying the exact price!")
 else:
 	print("Thanks for paying! Here's your change:")
 
-	for coin in coins.keys():
+	for coin in sorted(change, reverse=True):
 		count = change.count(coin)
 		if count > 0:
 			print(f"{count} x ${coin/100:.2f}")
